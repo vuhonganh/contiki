6d48f5e5a11ab73d6e1f6b6bfedc7f462b64089a
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-02-20 09:03:53 +0000

    add: read rssi value from received packetbuf

--- examples/cc26xx/cc26xx-demo.c
+++ examples/cc26xx/cc26xx-demo.c
@@ -53,8 +53,15 @@ AUTOSTART_PROCESSES(&example_broadcast_process);
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
+  /* original comment
   printf("broadcast message received from %d.%d: '%s'\n",
-         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+	 from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+  */
+  packetbuf_attr_t rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+  printf("broadcast message received from %d.%d: '%s', %d  \n",
+          from->u8[0], from->u8[1], (char *)packetbuf_dataptr(), 
+          rssi);
+
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
@@ -76,7 +83,7 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    packetbuf_copyfrom("Hello", 6);
+    packetbuf_copyfrom("who?", 6);
     broadcast_send(&broadcast);
     printf("broadcast message sent\n");
   }
