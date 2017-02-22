e567d35243e7b61de7b7e38e72f5c4343307cd66
Hong Anh VU <hong.vu16@imperial.ac.uk>
2017-02-22 12:41:25 +0000

    change rate and print avg rssi

--- examples/cc26xx/cc26xx-demo.c
+++ examples/cc26xx/cc26xx-demo.c
@@ -50,6 +50,9 @@
 PROCESS(example_broadcast_process, "Broadcast example");
 AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
+int reads[10];
+int cnt = 0;
+int avg = 0;
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
@@ -57,10 +60,25 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
   printf("broadcast message received from %d.%d: '%s'\n",
 	 from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
   */
-  packetbuf_attr_t rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
-  printf("broadcast message received from %d.%d: '%s', %d  \n",
+  // packetbuf_attr_t rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+  int rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI) - 65535;
+  printf("broadcast message received from %d.%d: '%s', \n%d\n",
           from->u8[0], from->u8[1], (char *)packetbuf_dataptr(), 
           rssi);
+  reads[cnt] = rssi;
+  cnt++;
+
+  if (cnt == 10)
+  {
+    cnt = 0;    
+    for (int i = 0; i < 10; i++)
+    {
+      avg += reads[i];
+    }
+    avg /= 10;
+    printf("avg = %d\n", avg);
+    avg = 0;
+  }
 
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
@@ -74,18 +92,19 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 
   PROCESS_BEGIN();
 
-  broadcast_open(&broadcast, 129, &broadcast_call);
+  broadcast_open(&broadcast, 140, &broadcast_call);
 
   while(1) {
 
     /* Delay 2-4 seconds */
-    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+    // etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+    etimer_set(&et, CLOCK_SECOND/10);
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
     packetbuf_copyfrom("who?", 6);
     broadcast_send(&broadcast);
-    printf("broadcast message sent\n");
+    //printf("broadcast message sent\n");
   }
 
   PROCESS_END();
